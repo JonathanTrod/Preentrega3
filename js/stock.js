216c67fc48acbce96883012944b01f67781a800a
Jonathan Trod <jonathantrod@hotmail.com>
2023-02-02 19:01:40 -0300

    Terminado

---
+++ js/stock.js
@@ -0,0 +1,142 @@
+// Array de objetos: se declara con const nombreArray=[]
+// Objeto: se declaran con {}. Cada objeto tiene propiedades y valores asociados a ellas.
+
+const productos = [
+  {
+    id: 1,
+    nombre: "Portatil HP",
+    imagen: "img/PORTATIL-LENOVO.jpg",
+    categoria: {
+      nombre: "portatil",
+      id: "portatil",
+    },
+    descripcion:
+      "Portatil. Capacidad total de almacenaje: 512GB. Resolucion de pantalla: 1920 x 1080 Pixeles",
+    precio: 210000,
+    cantidad: 1,
+  },
+
+  {
+    id: 2,
+    nombre: "Portatil ASUS",
+    imagen: "img/PORTATIL-LENOVO.jpg",
+    categoria: {
+      nombre: "portatiles",
+      id: "portatiles",
+    },
+    descripcion:
+      "Capacidad de almacenaje: 512GB. Resolucion de pantalla: 2880 x 1620 Pixeles",
+    precio: 346500,
+    cantidad: 1,
+  },
+
+  {
+    id: 3,
+    nombre: "Portatil LENOVO",
+    imagen: "img/PORTATIL-LENOVO.jpg",
+    categoria: {
+      nombre: "portatiles",
+      id: "portatiles",
+    },
+    descripcion:
+      "Capacidad de almacenaje: 512GB. Resolucion de pantalla: 1920 x 1080 Pixeles",
+    precio: 227500,
+    cantidad: 1,
+  },
+
+  {
+    id: 4,
+    nombre: "Portatil CHROMEBOOK",
+    imagen: "img/PORTATIL-LENOVO.jpg",
+    categoria: {
+      nombre: "portatiles",
+      id: "portatiles",
+    },
+    descripcion:
+      "Capacidad de almacenaje: 32GB. Resolucion de pantalla: 1366 x 768 Pixeles",
+    precio: 539500,
+    cantidad: 1,
+  },
+
+  {
+    id: 5,
+    nombre: "Celular SAMSUNG A32",
+    imagen: "img/PORTADA-smartphone.jpg",
+    categoria: {
+      nombre: "smartphones",
+      id: "smartphones",
+    },
+    descripcion:
+      "Modelo de procesador: 695. Capacidad de almacenamiento interno: 128GB",
+    precio: 91000,
+    cantidad: 1,
+  },
+
+  {
+    id: 6,
+    nombre: "Celular XIAOMI REDMI 10C",
+    imagen: "img/PORTADA-smartphone.jpg",
+    categoria: {
+      nombre: "smartphones",
+      id: "smartphones",
+    },
+    descripcion:
+      "Modelo de procesador: 680. Capacidad de almacenamiento interno: 128GB",
+    precio: 75950,
+    cantidad: 1,
+  },
+
+  {
+    id: 7,
+    nombre: "Celular IPhone 13",
+    imagen: "img/PORTADA-smartphone.jpg",
+    categoria: {
+      nombre: "smartphones",
+      id: "smartphones",
+    },
+    descripcion:
+      "Modelo del procesador: A15. Capacidad de almacenamiento interno: 128GB",
+    precio: 283500,
+    cantidad: 1,
+  },
+
+  {
+    id: 8,
+    nombre: "Celular IPhone 11",
+    imagen: "img/PORTADA-smartphone.jpg",
+    categoria: {
+      nombre: "smartphones",
+      id: "smartphones",
+    },
+    descripcion:
+      "Modelo de procesador: A13. Capacidad de almacenamiento interno: 64GB",
+    precio: 185150,
+    cantidad: 1,
+  },
+
+  {
+    id: 9,
+    nombre: "Auriculares",
+    imagen: "img/PORTADA-ACCESORIOS.jpg",
+    categoria: {
+      nombre: "accesorios",
+      id: "accesorios",
+    },
+    descripcion: "Audifonos: Intraaural. Tipo de conectividad: Alámbrico",
+    precio: 3500,
+    cantidad: 1,
+  },
+
+  {
+    id: 10,
+    nombre: "Protector de pantalla",
+    imagen: "img/PORTADA-ACCESORIOS.jpg",
+    categoria: {
+      nombre: "accesorios",
+      id: "accesorios",
+    },
+    descripcion: "Resistente a los golpes. Material: Vidrio templado",
+    precio: 1800,
+    cantidad: 1,
+  },
+];
